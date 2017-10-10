13a097ab3f9093e7731981c02e98d5e875dc4f49
DavyJohn <1139099003@qq.com>
2017-10-10 10:10:59 +0800

    Initial commit

---
+++ app/src/main/java/com/zzh/myscan/MainActivity.java
@@ -0,0 +1,48 @@
+package com.zzh.myscan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.zzh.myscan.utils.Constant;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        int mode = getIntent().getIntExtra(Constant.REQUEST_SCAN_MODE,Constant.REQUEST_SCAN_MODE_ALL_MODE);
+
+    }
+    @OnClick({R.id.create_code,R.id.scan_2code,R.id.scan_bar_code,R.id.scan_code})
+    public void click(View view){
+        Intent intent;
+        switch (view.getId()){
+            case R.id.create_code://生成吗
+                intent = new Intent(this,CreateCodeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.scan_2code://扫描二维码
+                intent = new Intent(this,CommonScanActivity.class);
+                intent.putExtra(Constant.REQUEST_SCAN_MODE,Constant.REQUEST_SCAN_MODE_QRCODE_MODE);
+                startActivity(intent);
+                break;
+            case R.id.scan_bar_code:
+                intent = new Intent(this,CommonScanActivity.class);
+                intent.putExtra(Constant.REQUEST_SCAN_MODE,Constant.REQUEST_SCAN_MODE_BARCODE_MODE);
+                startActivity(intent);
+                break;
+            case R.id.scan_code:
+                intent = new Intent(this,CommonScanActivity.class);
+                intent.putExtra(Constant.REQUEST_SCAN_MODE,Constant.REQUEST_SCAN_MODE_ALL_MODE);
+                startActivity(intent);
+                break;
+        }
+    }
+}
